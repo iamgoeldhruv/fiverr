4d0baf7a0b2fa1c6014b919f52d451234f527f26
iamgoeldhruv <goeldhruv9876@gmail.com>
2024-05-09 04:53:47 +0530

    tailwind setup and signup and login screen done

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Login from './pages/login';
+import Signin from './pages/signin';
+
+function App() {
+  return (
+   <Signin></Signin>
+   
+  );
+}
+
+export default App;
